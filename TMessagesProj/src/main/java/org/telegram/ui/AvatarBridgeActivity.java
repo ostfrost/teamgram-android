0f6c2704586fe344c3ac2baf1e7f954e7ddc0421
new android <newandroid2@a.com>
2026-07-14 22:33:39 +0700

    Avatar 1.0.5 - Update Avatar-module - status bar themed

--- TMessagesProj/src/main/java/org/telegram/ui/AvatarBridgeActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/AvatarBridgeActivity.java
@@ -1,10 +1,12 @@
 package org.telegram.ui;
 
+import android.content.res.Configuration;
 import android.content.Context;
 import android.view.View;
 
 import com.sousa.feature_avatar.bridge.AvatarExtension;
 
+import org.telegram.messenger.ApplicationLoader;
 import org.telegram.ui.ActionBar.BaseFragment;
 
 import kotlin.Unit;
@@ -25,6 +27,11 @@ public class AvatarBridgeActivity extends BaseFragment {
 
     @Override
     public boolean isLightStatusBar() {
-        return false;
+        Context context = getContext();
+        if (context == null) {
+            context = ApplicationLoader.applicationContext;
+        }
+        int currentNightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        return currentNightMode != Configuration.UI_MODE_NIGHT_YES;
     }
 }
